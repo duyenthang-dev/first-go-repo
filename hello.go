dd61975e4691717ef397cfe6d885293bd12e69da
Duyen Thang <duyenthang.dev@gmail.com>
2024-12-22 19:56:34 +0700

    second

--- hello.go
+++ hello.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+func changeForV2() int {
+	fmt.Print("Change for v2")
+	return 2
+}
+
 func main() {
 	x := 5
 
 	fmt.Println("Hello, World!", x)
+	fmt.Println("Hello, World!", changeForV2())
 }
